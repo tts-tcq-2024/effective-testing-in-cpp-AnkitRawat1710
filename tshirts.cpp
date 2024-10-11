c8474d888e97214cbe9f5c6452c5f918e1a114b5
Ankit Rawat <84120496+AnkitRawat1710@users.noreply.github.com>
2024-10-11 11:23:27 +0530

    Update tshirts.cpp

--- tshirts.cpp
+++ tshirts.cpp
@@ -16,8 +16,10 @@ char size(int cms) {
 int main() {
     assert(size(37) == 'S');
     assert(size(38) == 'S'); // Added to check cms == 38
+    
     assert(size(40) == 'M');
-    assert(size(42) == 'M'); // Added to check cms == 38
+    assert(size(42) == 'M'); // Added to check cms == 42
+    
     assert(size(43) == 'L');
     std::cout << "All is well (maybe!)\n";
     return 0;
