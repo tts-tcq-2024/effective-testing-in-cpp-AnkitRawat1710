5643d6fd374d9930419e6aec4051d5f01cee6182
Ankit Rawat <84120496+AnkitRawat1710@users.noreply.github.com>
2024-10-11 11:51:50 +0530

    Update alerter.cpp

--- alerter.cpp
+++ alerter.cpp
@@ -19,7 +19,7 @@ void alertInCelcius(float farenheit) {
         // let us keep a count of failures to report
         // However, this code doesn't count failures!
         // Add a test below to catch this bug. Alter the stub above, if needed.
-        alertFailureCount += 0;
+        alertFailureCount += 1;
     }
 }
 
