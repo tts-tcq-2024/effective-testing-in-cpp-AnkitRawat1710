028010cefc40921d6ced6140555de8eed049af3b
Ankit Rawat <84120496+AnkitRawat1710@users.noreply.github.com>
2024-10-18 11:40:41 +0530

    Update misaligned.cpp

--- misaligned.cpp
+++ misaligned.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <assert.h>
 
+//Typedef for a print function
+typedef void(*PrintFunc)(int, const char*, const char*);
 
 int printColorMap() {
     const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
@@ -8,11 +10,17 @@ int printColorMap() {
     int i = 0, j = 0;
     for(i = 0; i < 5; i++) {
         for(j = 0; j < 5; j++) {
-            std::cout << i * 5 + j << " | " << majorColor[i] << " | " << minorColor[i] << "\n";
+            printFunc(i * 5 + j, majorColor[i], minorColor[j]);
         }
     }
     return i * j;
 }
+
+//Real print function
+void realPrint(int pairNumber, const char* major, const char* minor){
+std::cout << i * 5 + j << " | " << majorColor[i] << " | " << minorColor[i] << "\n";
+}
+
 
 int main() {
     int result = printColorMap();
